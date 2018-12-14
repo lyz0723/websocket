674eac814ebbe5c9f919b645e2d5cb90cbf8a607
weixin <360196045@qq.com>
2018-12-14 16:04:04 +0800

    redis

--- aa.php
+++ aa.php
@@ -1,5 +1,5 @@
 <?php
-exec("netstat -ano|findstr 192.168.1.97:8000",$out);
+/*exec("netstat -ano|findstr 192.168.1.97:8000",$out);
 $a=explode(' ',$out[0]);
 $arr=array();
 foreach($a as $k=>$v){
@@ -11,4 +11,11 @@ foreach($a as $k=>$v){
 }
 $pid=$arr[4];
 exec("taskkill /pid $pid -f",$b);
-print_r($b);
+print_r($b);*/
+include_once 'redis.php';
+$config = array(
+    'port'=> 6379,
+    'host'=> '127.0.0.1',
+);
+$redis = new Redis($config);
+print_r($redis);
